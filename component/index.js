5d631347d0231b79fe5889d67b7ce65cfe35fd1b
epayet <jakspinning@gmail.com>
2015-04-16 17:37:05 +0200

    correct writing function

--- component/index.js
+++ component/index.js
@@ -5,85 +5,83 @@ var yosay = require('yosay');
 var fs = require('fs');
 
 module.exports = yeoman.generators.NamedBase.extend({
-  prompting: function () {
-    var done = this.async();
+    prompting: function () {
+        var done = this.async();
 
-    // Have Yeoman greet the user.
-    //this.log(yosay(
-    //  'Welcome to the funkadelic ' + chalk.red('Angular ES6 Components') + ' generator!'
-    //));
+        // Have Yeoman greet the user.
+        //this.log(yosay(
+        //  'Welcome to the funkadelic ' + chalk.red('Angular ES6 Components') + ' generator!'
+        //));
 
-    //getDirs(__dirname + '/app/components', function (modules) {
-    //  console.log(modules);
-    //});
+        //getDirs(__dirname + '/app/components', function (modules) {
+        //  console.log(modules);
+        //});
 
 
+        var prompts = [
+            //  {
+            //  type: 'String',
+            //  name: 'path',
+            //  message: 'Where would you like to create the component?',
+            //  default: 'app/components'
+            //}
+        ];
 
-    var prompts = [
-    //  {
-    //  type: 'String',
-    //  name: 'path',
-    //  message: 'Where would you like to create the component?',
-    //  default: 'app/components'
-    //}
-    ];
+        this.prompt(prompts, function (props) {
+            this.props = props;
+            // To access props later use this.props.someOption;
 
-    this.prompt(prompts, function (props) {
-      this.props = props;
-      // To access props later use this.props.someOption;
+            done();
+        }.bind(this));
+    },
 
-      done();
-    }.bind(this));
-  },
+    writing: function () {
+        var name = this._args[0];
+        var nameWithCaps = name;
+        nameWithCaps[0] = nameWithCaps[0].toUpperCase();
 
-  writing: {
-    app: function () {
-      var name = this._args[0];
-      var nameWithCaps = name;
-      nameWithCaps[0] = nameWithCaps[0].toUpperCase();
-
-      var args = {componentNameCaps: nameWithCaps, componentName: name};
-      this.fs.copy(
-        this.templatePath('_module.js'),
-        this.destinationPath(name + '.js'),
-        args
-      );
-      this.fs.copy(
-          this.templatePath('_controller.js'),
-          this.destinationPath(name + '.controller.js'),
-          args
-      );
-      this.fs.copy(
-          this.templatePath('_controller.spec.js'),
-          this.destinationPath(name + '.controller.spec.js'),
-          args
-      );
-      this.fs.copy(
-          this.templatePath('_service.js'),
-          this.destinationPath(name + '.service.js'),
-          args
-      );
-      this.fs.copy(
-          this.templatePath('_service.spec.js'),
-          this.destinationPath(name + '.service.spec.js'),
-          args
-      );
-      this.fs.copy(
-          this.templatePath('_directive.js'),
-          this.destinationPath(name + '.directive.js'),
-          args
-      );
-      this.fs.copy(
-          this.templatePath('_html.html'),
-          this.destinationPath(name + '.html')
-      );
-      this.fs.copy(
-          this.templatePath('_css.css'),
-          this.destinationPath(name + '.css')
-      );
+        var args = {componentNameCaps: nameWithCaps, componentName: name};
+        this.fs.copy(
+            this.templatePath('_module.js'),
+            this.destinationPath(name + '.js'),
+            args
+        );
+        this.fs.copy(
+            this.templatePath('_controller.js'),
+            this.destinationPath(name + '.controller.js'),
+            args
+        );
+        this.fs.copy(
+            this.templatePath('_controller.spec.js'),
+            this.destinationPath(name + '.controller.spec.js'),
+            args
+        );
+        this.fs.copy(
+            this.templatePath('_service.js'),
+            this.destinationPath(name + '.service.js'),
+            args
+        );
+        this.fs.copy(
+            this.templatePath('_service.spec.js'),
+            this.destinationPath(name + '.service.spec.js'),
+            args
+        );
+        this.fs.copy(
+            this.templatePath('_directive.js'),
+            this.destinationPath(name + '.directive.js'),
+            args
+        );
+        this.fs.copy(
+            this.templatePath('_html.html'),
+            this.destinationPath(name + '.html')
+        );
+        this.fs.copy(
+            this.templatePath('_css.css'),
+            this.destinationPath(name + '.css')
+        );
     }
-  }
 });
+
 
 var getDirs = function (rootDir, cb) {
   fs.readdir(rootDir, function (err, files) {
